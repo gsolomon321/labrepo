69197c2396938124ad6640576fb7d4b65ad9fc83
gsolomon321 <vmv8gp@virginia.edu>
2026-01-16 20:55:07 +0000

    recipe

--- recipe.py
+++ recipe.py
@@ -42,4 +42,6 @@
 # the following commands into the terminal: 
 #1. `git add .`
 #2. `git commit -m ‘personalized message/changes made’`
-#3. `git push origin main``
+#3. `git push origin main`
+
+# %%
